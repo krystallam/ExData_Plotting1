0bf7cb86e2188ff5cb16b28f3cfa06dc9a75b03a
Krystal Lam <krystal.l@hotmail.com>
2015-08-09 13:40:27 -0400

    add comment
    add comment

--- plot4.R
+++ plot4.R
@@ -15,16 +15,16 @@ png(filename = "plot4.png",width = 480, height = 480,units = "px", bg = "transpa
 
 par(mfrow = c(2, 2)) ###set the final plot as 2X2 format
 
-plot(datetime, mydata_sub$Global_active_power, type="l", xlab="", ylab="Global Active Power")
+plot(datetime, mydata_sub$Global_active_power, type="l", xlab="", ylab="Global Active Power") ###top left plot
 
-plot(datetime,mydata_sub$Voltage, type="l", xlab="datetime", ylab="Voltage")
+plot(datetime,mydata_sub$Voltage, type="l", xlab="datetime", ylab="Voltage") ###top right plot
 
-plot(datetime,mydata_sub$Sub_metering_1,type = "n",ylab="Energy sub metering", xlab="", )
+plot(datetime,mydata_sub$Sub_metering_1,type = "n",ylab="Energy sub metering", xlab="", ) ###bottom left plot
 lines(datetime,mydata_sub$Sub_metering_1, col = "black")
 lines(datetime,mydata_sub$Sub_metering_2, col = "red")
 lines(datetime,mydata_sub$Sub_metering_3, col = "blue")
 legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=1, lwd=2.5, col=c("black", "red", "blue"),bty="n")
 
-plot(datetime,mydata_sub$Global_reactive_power, type="l", xlab="datetime", ylab="Global_reactive_power")
+plot(datetime,mydata_sub$Global_reactive_power, type="l", xlab="datetime", ylab="Global_reactive_power")  ###bottom right plot
 
 dev.off()
